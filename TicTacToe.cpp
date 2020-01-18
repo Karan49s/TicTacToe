2effaa8612f85247a55f10a6dff0f4d2ecb60bc7
karan49s <karan.4sep@gmail.com>
2020-01-18 18:42:31 +0530

    menu

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,22 +4,121 @@
 #include <iostream>
 #include <cstdlib>
 #include<vector>
+#include <conio.h>
 using namespace std;
 
 //array to store the taken positions
 char board[] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ' };
+
+
+void PrintGameTitle()
+{
+	system("CLS");
+	cout << "XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOX\n";
+	cout << "OXO   TICTACTOE USING C++   OXO\n";
+	cout << "XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOX\n\n";
+}
+
+void PrintModeMenu(int x)
+{
+	if (x == 0)
+	{
+		cout << "### SINGLE PLAYER (v/s CPU) ###\n";
+		cout << "          MULTIPLAYER\n";
+	}
+	else
+	{
+		cout << "    SINGLE PLAYER (v/s CPU)\n";
+		cout << "###       MULTIPLAYER       ###\n";
+	}
+}
+
+void PrintDifficultyMenu(int x)
+{
+	if (x == 0)
+	{
+		cout << "####  EASY  ####\n";
+		cout << "     NORMAL\n";
+		cout << "      HARD\n";
+	}
+	else if (x == 1)
+	{
+		cout << "      EASY\n";
+		cout << "#### NORMAL ####\n";
+		cout << "      HARD\n";
+	}
+	else
+	{
+		cout << "      EASY\n";
+		cout << "     NORMAL\n";
+		cout << "####  HARD  ####\n";
+	}
+}
+
+void PrintNavigationInstructions()
+{
+	cout << "\n\n---------------------------------------\n";
+	cout << "# navigate through up and down keys \n";
+	cout << "# press enter to select\n";
+}
+
+vector<int> menu()
+{
+	vector<int> mode_difficulty;
+	int key = 0;
+	int pos = 0;
+	PrintGameTitle();
+	PrintModeMenu(pos % 2);
+	PrintNavigationInstructions();
+	while (key != 13)
+	{
+		key = _getch();
+		key = toupper(key);
+		if (key == 72) pos--;
+		else if (key == 80) pos++;
+		PrintGameTitle();
+		PrintModeMenu(pos % 2);
+		PrintNavigationInstructions();
+	}
+	mode_difficulty.push_back(pos % 2);
+
+	if (pos % 2 == 1) return mode_difficulty;
+
+	key = 0;
+	pos = 0;
+	PrintGameTitle();
+	PrintDifficultyMenu(pos % 3);
+	PrintNavigationInstructions();
+
+	while (key != 13)
+	{
+		key = _getch();
+		key = toupper(key);
+		if (key == 72) pos--;
+		else if (key == 80) pos++;
+		system("CLS");
+		PrintGameTitle();
+		PrintDifficultyMenu(pos % 3);
+	}
+	mode_difficulty.push_back(pos % 3);
+
+
+
+	return mode_difficulty;
+}
+
+
 
 ///////////////////////////////////////////
 //function to print the tic tac toe board//
 ///////////////////////////////////////////
 void printBoard()
 {
-	system("CLS");
-	cout << "-+-+-+-+-TicTacToe-+-+-+-+-\n\n";
-	cout <<"\t"<< board[1] << " | " << board[2] << " | " <<board[3] << "\n";
+	PrintGameTitle();
+	cout << "\t"<< board[1] << " | " << board[2] << " | " <<board[3] << "\n";
 	cout << "\t- + - + -\n";
 	cout << "\t" << board[4] << " | " << board[5] << " | " << board[6] << "\n";
-	cout <<  "\t- + - + -\n";
+	cout << "\t- + - + -\n";
 	cout << "\t" << board[7] << " | " << board[8] << " | " << board[9] << "\n";
 }
 
@@ -27,22 +126,45 @@ void printBoard()
 //inputplayer function                                              //    
 //to take input from player and repeat untill a empty place is found//
 //////////////////////////////////////////////////////////////////////
-void inputplayer()
+void inputplayer1()
 {
 	cout << "enter your position to place your mark (1-9)(you are X)";
 	int p;
-	cin >> p;
+	//cin >> p;
+	p = _getch();
+	p = toupper(p)-48;
 	if (board[p] == 'X' || board[p] == 'O')
 	{
-		cout << "position is already taken by"<< (board[p]=='O'?"cpu\n":"you\n") <<"Please enter a empty position\n";
-		inputplayer();
+		cout << "position is already taken by"<< (board[p]=='O'?"player 1\n":"you\n") <<"Please enter a empty position\n";
+		inputplayer1();
 	}
 	else
 		board[p] = 'X';
 
 }
+//////////////////////////////////////////////////////////////////////
+//inputplayer function                                              //    
+//to take input from player and repeat untill a empty place is found//
+//////////////////////////////////////////////////////////////////////
+void inputplayer2()
+{
+	cout << "enter your position to place your mark (1-9)(you are O)";
+	int p;
+	//cin >> p;
+	p = _getch();
+	p = toupper(p) - 48;
+	if (board[p] == 'X' || board[p] == 'O')
+	{
+		cout << "position is already taken by" << (board[p] == 'O' ? "player 2\n" : "you\n") << "Please enter a empty position\n";
+		inputplayer2();
+	}
+	else
+		board[p] = 'O';
+
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////
-//iswinner finction to check if we have a winner and returns values according to winner//
+//iswinner function to check if we have a winner and returns values according to winner//
 /////////////////////////////////////////////////////////////////////////////////////////
 int iswinner(char board[])
 {
@@ -113,37 +235,56 @@ void cpuTurn()
 
 }
 
-
-
-
 int main()
 {
-	bool turn=rand()%2; //1 for player and 0 for cpu
-	printBoard();
-	int turncnt = 0;
-	while (!iswinner(board) && turncnt<9)
+	vector<int> A;
+	int choice=1;
+	while (choice !=0 )
 	{
-		if (turn == 0) //checking turn and changing turn
+		A = menu();
+		if (A[0] == 0)                              //single player
 		{
-			cpuTurn(); 
-			turn = 1;
-			turncnt++;
+			printBoard();
+			int turncnt = 0;
+			int difficulty = 0;
+			bool turn = rand() % 2; //1 for player and 0 for cpu
+
+			while (!iswinner(board) && turncnt < 9)
+			{
+				if (turn == 0) //checking turn and changing turn
+				{
+					cpuTurn();
+					turn = 1;
+					turncnt++;
+				}
+				else
+				{
+					inputplayer1();
+					turn = 0;
+					turncnt++;
+				}
+				printBoard();
+				if (iswinner(board) == 1) cout << "\n\tYOU WIN :)\n";
+				else if (iswinner(board)) cout << "\n\tYOU LOSE! :(\n\t\tCPU WINS \n";
+
+			}
+			if (turncnt == 9 && iswinner(board) == 0)
+			{
+				cout << "\n\t-+-+-+-+-+-+-+DRAW+-+-+-+-+-+-+-+-";
+			}
+
 		}
-		else
+		else //multiplayer
 		{
-			inputplayer();
-			turn = 0;
-			turncnt++;
+			printBoard();
 		}
-		printBoard();
-		if (iswinner(board) == 1) cout << "\n\tYOU WIN :)\n";
-		else if(iswinner(board)) cout << "\n\tYOU LOSE! :(\n\t\tCPU WINS \n";
-		
+
+
+		cout << "DO YOU WANT TO PLAY AGAIN\n";
+		cin >> choice;
 	}
-	if (turncnt == 9 && iswinner(board) == 0)
-	{
-		cout << "\n\t-+-+-+-+-+-+-+DRAW+-+-+-+-+-+-+-+-";
-	}
+	
+	
 	
 
     
